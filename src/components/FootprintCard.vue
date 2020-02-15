91be30531f3b8071d32e3cd0123657a2b774b756
therealtimhawkins <timothyhaydenhawkins@gmail.com>
2020-02-15 22:09:44 +0000

    adds footprint table header

--- src/components/FootprintCard.vue
+++ src/components/FootprintCard.vue
@@ -1,82 +1,89 @@
 <template>
-  <div class="column is-12">
-    <div class="box">
-      <div class="columns is-vcentered">
-        <div class="column is-narrow">{{ date }}</div>
-        <div class="column is-narrow">
-          <div
-            class="tag is-link is-light is-medium has-text-weight-bold is-uppercase"
-          >
-            {{ footprint.name }}
-          </div>
+  <div class="box">
+    <div class="columns is-vcentered">
+      <div class="column is-narrow">{{ date }}</div>
+      <div class="column is-narrow">
+        <div
+          class="tag is-link is-light is-medium has-text-weight-bold is-uppercase"
+        >
+          {{ footprint.name }}
         </div>
-        <div class="column">{{ itemQuantity }} items</div>
-        <div class="column is-narrow">
-          <div class="field has-addons">
+      </div>
+      <div class="column">{{ itemQuantity }} items</div>
+      <div class="column is-narrow">
+        <div class="field has-addons">
+          <p class="control">
             <button class="button is-link is-light">
-              <p class="control">
-                <i class="fa fa-pound-sign"></i><span class="">4.90</span>
-              </p>
+              <span class="icon is-small">
+                <i class="fas fa-pound-sign"></i>
+              </span>
+              <span>4.90</span>
             </button>
-            <p class="control">
-              <button class="button is-link is-light">
-                <i class="fa fa-tree"></i><span class="">1.7</span>
-              </button>
-            </p>
-            <p class="control">
-              <button class="button is-link is-light">
-                <i class="fa fa-gas-pump"></i><span class="">84</span>
-              </button>
-            </p>
-          </div>
+          </p>
+          <p class="control">
+            <button class="button is-link is-light">
+              <span class="icon is-small">
+                <i class="fas fa-tree"></i>
+              </span>
+              <span>1.7</span>
+            </button>
+          </p>
+          <p class="control">
+            <button class="button is-link is-light">
+              <span class="icon is-small">
+                <i class="fas fa-gas-pump"></i>
+              </span>
+              <span>84</span>
+            </button>
+          </p>
         </div>
-        <div class="column is-narrow">
-          <div class="field has-addons">
-            <p class="control">
-              <button class="button is-link is-light">
-                <a
-                  href="mailto:${contact.email}?subject=Your Carbon Footprint&body=${email}"
-                  class="icon"
-                  data-show-count="false"
-                  target="_blank"
-                >
-                  <i class="fas fa-2x fa-envelope-square"></i>
-                </a>
-              </button>
-            </p>
-            <p class="control">
-              <button class="button is-link is-light">
-                <a
-                  href="https://twitter.com/intent/tweet?button_hashtag=ClimateCrisis&ref_src=twsrc%5Etfw&text=${tweet}"
-                  class="icon"
-                  data-show-count="false"
-                  target="_blank"
-                >
-                  <i class="fab fa-2x fa-twitter-square"></i>
-                </a>
-              </button>
-            </p>
-            <p class="control">
-              <button class="button is-link is-light">
-                <a
-                  class="icon"
-                  href="https://www.facebook.com/sharer/sharer.php?u=example.org"
-                  target="_blank"
-                >
-                  <i class="fab fa-2x fa-facebook-square"></i>
-                </a>
-              </button>
-            </p>
-          </div>
+      </div>
+      <div class="column is-narrow">
+        <div class="field has-addons">
+          <p class="control">
+            <button class="button is-link is-light">
+              <a
+                href="mailto:${contact.email}?subject=Your Carbon Footprint&body=${email}"
+                class="icon"
+                data-show-count="false"
+                target="_blank"
+              >
+                <i class="fas fa-2x fa-envelope-square"></i>
+              </a>
+            </button>
+          </p>
+          <p class="control">
+            <button class="button is-link is-light">
+              <a
+                href="https://twitter.com/intent/tweet?button_hashtag=ClimateCrisis&ref_src=twsrc%5Etfw&text=${tweet}"
+                class="icon"
+                data-show-count="false"
+                target="_blank"
+              >
+                <i class="fab fa-2x fa-twitter-square"></i>
+              </a>
+            </button>
+          </p>
+          <p class="control">
+            <button class="button is-link is-light">
+              <a
+                class="icon"
+                href="https://www.facebook.com/sharer/sharer.php?u=example.org"
+                target="_blank"
+              >
+                <i class="fab fa-2x fa-facebook-square"></i>
+              </a>
+            </button>
+          </p>
         </div>
-        <div class="column is-narrow">
-          <button class="button is-link is-light">
-            <span class="icon is-small">
-              <i class="fas fa-chart-pie"></i>
-            </span>
-            <span>Graph view</span>
-          </button>
-        </div>
+      </div>
+      <div class="column is-narrow">
+        <button class="button is-link is-light">
+          <span class="icon is-small">
+            <i class="fas fa-chart-pie"></i>
+          </span>
+          <span>Graph view</span>
+        </button>
       </div>
     </div>
   </div>
--- src/components/FootprintsTable.vue
+++ src/components/FootprintsTable.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="section">
     <div class="container">
-      <div class="columns is-multiline" v-for="footprint in footprints" v-bind:key="footprint.time">
+      <div class="columns is-multiline"></div>
+      <div class="column is-12">
+        <div class="columns is-size-5">
+          <div class="column is-narrow has-text-weight-bold has-text-primary">
+            <i class="fas fa-shoe-prints"></i>
+            {{ footprints.length }} footprints in saved
+          </div>
+          <div class="column"></div>
+          <!-- <div class="column is-narrow has-text-link">
+            Average listing price of listings
+          </div> -->
+        </div>
+      </div>
+      <div
+        class="column is-12"
+        v-for="footprint in footprints"
+        v-bind:key="footprint.time"
+      >
         <FootprintCard :footprint="footprint" />
       </div>
     </div>
@@ -9,7 +26,7 @@
 </template>
 
 <script>
-import FootprintCard from "./FootprintCard";
+import FootprintCard from "./FootprintCard"
 
 export default {
   name: "FootprintsTable",
@@ -22,5 +39,5 @@ export default {
       required: true
     }
   }
-};
+}
 </script>
