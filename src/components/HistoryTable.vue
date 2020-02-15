92c8dbb2259340f89c0bdd0dfaaef032a4f0f128
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2020-02-15 22:15:03 +0000

    adds greenpay logo

---
+++ src/components/HistoryTable.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <div class="columns is-multiline">
+        <div
+          class="column is-12"
+          v-for="(footprint, index) in history"
+          v-bind:key="footprint.name + index"
+        >
+          <div class="box">
+            <div class="columns">
+              <div class="column">{{footprint.name}}</div>
+              <div class="column"></div>
+              <div class="column"></div>
+              <div class="column"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HistoryTable",
+  components: {},
+  props: {
+    history: {
+      type: Array,
+      default: () => [
+        {
+          name: "selfridges",
+          items: [
+            {
+              name: "ALLSAINTS",
+              description: "Drury camouflage-effect leather jacket",
+              price: "\n\t\t\t\t£190.00\n\t\t\t",
+              size: "Size:  M"
+            },
+            {
+              name: "OFF-WHITE C/O VIRGIL ABLOH",
+              description: "Ocean Debris long-sleeved cotton-jersey T-shirt",
+              price: "\n\t\t\t\t£245.00\n\t\t\t",
+              size: "Size:  M"
+            },
+            {
+              name: "STONE ISLAND",
+              description: "Double-zipped cotton-jersey hoody",
+              price: "\n\t\t\t\t£350.00\n\t\t\t",
+              size: "Size:  XL"
+            }
+          ],
+          time: 1580763289141
+        },
+        {
+          name: "selfridges",
+          items: [
+            {
+              name: "ALLSAINTS",
+              description: "Drury camouflage-effect leather jacket",
+              price: "\n\t\t\t\t£190.00\n\t\t\t",
+              size: "Size:  M"
+            },
+            {
+              name: "OFF-WHITE C/O VIRGIL ABLOH",
+              description: "Ocean Debris long-sleeved cotton-jersey T-shirt",
+              price: "\n\t\t\t\t£245.00\n\t\t\t",
+              size: "Size:  M"
+            },
+            {
+              name: "STONE ISLAND",
+              description: "Double-zipped cotton-jersey hoody",
+              price: "\n\t\t\t\t£350.00\n\t\t\t",
+              size: "Size:  XL"
+            }
+          ],
+          time: 1580763289141
+        }
+      ]
+    }
+  }
+};
+</script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,24 +1,21 @@
 <template>
   <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="/"> </a>
-
-      <a
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
+    <div class="title is-4 is-centered has-text-white" id="logo">GreenPay.</div>
   </nav>
 </template>
 
 <script>
 export default {
   name: "NavBar"
-}
+};
 </script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Playfair+Display:900:italic&display=swap");
+
+#logo {
+  font-family: "Playfair Display", serif, italic;
+  font-weight: 900;
+  padding: 8px 0 0 12px;
+}
+</style>
